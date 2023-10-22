a41359edfd55fe41a8b3b57ad76f8b5bf69fd645
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-22 23:03:22 +0000

    very good

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
+import Layout from "./layout/Layout.jsx"
 
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <Layout />
   )
 }
 
--- Ecommerce/src/routers/Routers.jsx
+++ Ecommerce/src/routers/Routers.jsx
@@ -15,6 +15,7 @@ import Signup from "../pages/Signup"
 const Routers = () => {
   return (
     <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/shop/:id' element={<ProductDetails />} />
