5cf6fd4fe5f059ca0a7d7005c83c76e126831778
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-22 23:15:42 +0000

    short

--- Ecommerce/src/components/Header/Header.jsx
+++ Ecommerce/src/components/Header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import logo from "../../assets/images/eco-logo.png"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+        <div className="container">
+            <div className="row">
+                <div className="nav__wrapper">
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                        <div>
+                            <h1>Multimart</h1>
+                            <p>Since 1995</p>
+                        </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </header>
   )
 }
 
