694b166fcb6523074703e8cf88fda40af9cc8f7c
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-23 23:21:27 +0000

    short

--- Ecommerce/src/components/Header/Header.jsx
+++ Ecommerce/src/components/Header/Header.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
+import { Container, Row } from "reactstrap"
 
 import logo from "../../assets/images/eco-logo.png"
 
 const Header = () => {
   return (
     <header className="header">
-        <div className="container">
-            <div className="row">
-                <div className="nav__wrapper">
-                    <div className="logo">
-                        <img src={logo} alt="logo" />
-                        <div>
-                            <h1>Multimart</h1>
-                            <p>Since 1995</p>
-                        </div>
-                    </div>
-
-                    
+        <Container>
+            <Row>
+               <div className="nav__wrapper">
+                <div className="logo">
+                    <img src={logo} alt="" />
                 </div>
-            </div>
-        </div>
+               </div>
+            </Row>
+        </Container>
     </header>
   )
 }
